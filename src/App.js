e5d7e09b8e50fc44d87d48a1bdb841445b97cbce
paramesh <maddukuri.kumar@bizongo.com>
2023-04-14 13:49:30 +0530

    added ga4-library to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Input ,Typography, Menu } from 'antd';
 import tick from './Tickmark.svg';
 
 import imag from './Bannerimage.svg';
-import { Col, Row } from 'antd';
+
 import { Button } from 'antd';
 
 import { useState } from 'react';
-
-
-import DropdownInput from './Dropdown.js';
+import {handleClick} from '@smartpaddle/biz-bamboo.ui.ga4-dev'
+import {Add_to_Cart} from '@smartpaddle/biz-bamboo.ui.hello-world'
+// import DropdownInput from './Dropdown.js';
 import Cards from './Card';
 import { Layout } from 'antd';
 
+import ReactGA from 'react-ga';
+
+
+ReactGA.initialize('G-R6M3PRGD7C');
 
 const { Header} = Layout;
 let obj = [
-  { total : 1000000,
+  { total : 10000000,
    profit : 5,
    return : 5, 
    debt : 3, 
@@ -27,16 +31,21 @@ let obj = [
 
 const { Title ,Paragraph } = Typography;
 function App() {
-   
-   
-  
-   let [boolval , setboolval] = useState (false);
-   function submithandler  ()  {
+  var now = new Date();
+   let  var1= window.location.href;
+    // let time  = now.toLocaleString().slice(0, 19).replace('T', ' ');
+  // let var1 = 'button';
+  let var2 = 'hi';
+  function inputHandler () {
+    console.log ("inputting ")
+    window.gtag('event', 'user_input_start', {
+      'event_category': 'User Input',
+      'event_label': 'User started inputting',
+    });
+  }
 
-      setboolval(true);
-        
-   }
-   
+  
+   let [submitValue , setSubmitValue] =useState('false');
    
   return ( 
   
@@ -47,7 +56,7 @@ function App() {
    {/* <div className='black'>
    
   </div> */}
-  <Layout >
+  <Layout  >
     <Header >
      
       <Menu
@@ -74,11 +83,11 @@ function App() {
 
   
    
-   <Paragraph className='head'>Please provide your MCA issued CIN (Company Identification Number)</Paragraph>
+   <Paragraph className='head' onmouseover>Please provide your MCA issued CIN (Company Identification Number)</Paragraph>
    
     
    
-   <Input className='input' maxLength={21} 
+   <Input className='input' onInput={inputHandler()} maxLength={21} 
 		placeholder="Enter 21-Digit-CIN" />
     <div className='circle'>
    <img  src={tick} alt='not found' />
@@ -86,23 +95,25 @@ function App() {
    </div>
   
   
-    <DropdownInput/>
+    {/* <DropdownInput/> */}
 
   
 
 
-
+<a  className = "button" id='buttto'  style={{ positon : 'absolute'  , right : '40px'}} href='https://github.com/orgs/Bizongo/repositories' >link</a>
 
 
      <div className='button1'>
      
-     <Button  className = "button" type="primary" size={'large'} onSubmit={submithandler} shape = "default" style={{ borderRadius: 0 }} >Proceed</Button>
+     <Button  className = "button" onClick={(event)=>handleClick({ui : var1 , ctc:var2})} type="primary"  size={'large'}  shape = "default" style={{ borderRadius: 0 }} >Proceed</Button>
+     
      </div>
      <div className='card'>
       
       
       <Cards obj = {obj} />
      
+    
       
     
       
